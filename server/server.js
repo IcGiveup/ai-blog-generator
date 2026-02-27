8b0405697ecdbe8bb557fe672563755a416c533d
Zyif <fiyazahmd002@gmail.com>
2026-02-28 05:20:40 +0600

    deployment

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 app.use("/api/ai", aiRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
